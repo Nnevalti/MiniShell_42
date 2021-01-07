b29f1b34097bb60036e99f2d34cbf5142bf7f3ff
deschamps.valentin.1998@gmail.com <deschamps.valentin.1998@gmail.com>
2021-01-07 16:56:33 +0100

    Cleaned libft dir and makefile

---
+++ srcs/libft/srcs/ft_strdup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfevrier <tfevrier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/07 13:53:30 by tfevrier          #+#    #+#             */
+/*   Updated: 2019/10/08 11:10:52 by tfevrier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "../libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	char	*copy;
+	int		i;
+
+	copy = (char *)malloc((ft_strlen((char *)str) + 1) * sizeof(char));
+	i = 0;
+	while (str[i])
+	{
+		copy[i] = str[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
