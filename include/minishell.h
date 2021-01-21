0e07060e2de1b1689d4af228a9edfa80f414fc03
hvernhes <hugovernhes@yahoo.fr>
2021-01-21 15:47:43 +0100

    error struct

--- include/minishell.h
+++ include/minishell.h
@@ -23,32 +23,41 @@
 # include "../libft/include/libft.h"
 # include "../libft/include/get_next_line.h"
 
-# define MAX_PATH_LENGTH 4096
+# define 			MAX_PATH_LENGTH 4096
 
-# define TRUE 1
-# define FALSE 0
+# define 			TRUE 1
+# define 			FALSE 0
 
-int				EXIT_CODE;
+int					EXIT_CODE;
 
-typedef int		t_bool;
+typedef int			t_bool;
 
 
-typedef enum	e_type
+typedef enum		e_type
 {
 	SEMI_COLON,
 	PIPE,
 	REDIR,
 	COMMAND,
 	DEFAULT
-}				t_type;
+}					t_type;
 
-typedef enum	e_redir_type
+typedef enum		e_redir_type
 {
 	NONE,
 	REDIRECT_STDOUT,
 	APPEND_STDOUT,
 	REDIRECT_STDIN
-}				t_redir_type;
+}					t_redir_type;
+
+typedef enum		e_errno
+{
+	NONE,
+	QUOTE,
+	PARSER,
+	MALLOC
+
+}					t_erno;
 
 typedef struct		s_tree
 {
@@ -61,39 +70,52 @@ typedef struct		s_tree
 	void			*right; //		ls			sort
 }					t_tree;
 
-typedef	struct	s_redirection
+typedef	struct		s_redirection
 {
 	char			*str;
 	t_redir_type	type;
 	int				saved_stdout;
-}				t_redirection;
+}					t_redirection;
 
-int				ft_env(char **env);
-void			ft_pwd(void);
-void			ft_echo(char **env, char *command);
-void			ft_cd(char *path);
-void			ft_export(char ***env, char **splitted);
-void			ft_unset(char ***env, char **splitted);
+typedef struct		s_error
+{
+	t_errno			errno;
+	char			*value;
+}					t_error;
 
-int				ft_indexof(char *str, char c);
-char			**ft_lexer(char const *commands);
-char			*handle_env(char **env, char const *str);
+typedef struct		s_data
+{
+	char			**my_env;
+	t_tree			*tree;
+	char			*command;
+}					t_data;
 
-char			**get_env(char **env);
-char 			*get_env_var(char **env, char *name);
-t_bool			set_env_var(char **env, char *name, char *variable);
+int					ft_env(char **env);
+void				ft_pwd(void);
+void				ft_echo(char **env, char *command);
+void				ft_cd(char *path);
+void				ft_export(char ***env, char **splitted);
+void				ft_unset(char ***env, char **splitted);
 
-int				tab_str_len(char **tab);
-void			free_tab_str(char **tab);
+int					ft_indexof(char *str, char c);
+char				**ft_lexer(char const *commands);
+char				*handle_env(char **env, char const *str);
 
-t_redirection	**set_redirections(char *command);
-void			reset_redirections(t_redirection **redirections);
+char				**get_env(char **env);
+char 				*get_env_var(char **env, char *name);
+t_bool				set_env_var(char **env, char *name, char *variable);
 
-int 			*init_pipes(char **tokens);
-void			pipe_io(int *pipes, int index);
-void			close_pipes(int *pipes);
-void			ast_exec(t_tree *entry);
-void			free_ast(t_tree *entry);
-t_tree			*ft_parser(char **commands);
+int					tab_str_len(char **tab);
+void				free_tab_str(char **tab);
+
+t_redirection		**set_redirections(char *command);
+void				reset_redirections(t_redirection **redirections);
+
+int 				*init_pipes(char **tokens);
+void				pipe_io(int *pipes, int index);
+void				close_pipes(int *pipes);
+void				ast_exec(t_tree *entry);
+void				free_ast(t_tree *entry);
+t_tree				*ft_parser(char **commands);
 
 #endif
